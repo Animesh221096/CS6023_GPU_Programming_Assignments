accac0d376973054d55bf9eeeabc72d28d1a9ea6
Animesh221096 <animeshjain221096@gmail.com>
2026-08-08 17:00:04 +0530

    Edit Submit Script

--- Assignment_1/submit.cmd
+++ Assignment_1/submit.cmd
@@ -4,6 +4,9 @@
 #PBS -l select=1:ncpus=1:ngpus=1
 #PBS -q gpuq
 
+module load gcc/8.5.0
+module load python/3.9.0
+
 tpdir=`echo $PBS_JOBID | cut -f 1 -d .`
 tempdir=$HOME/scratch/job$tpdir
 mkdir -p $tempdir
@@ -11,5 +14,5 @@ cd $tempdir
 cp -R $PBS_O_WORKDIR/* .
 ./compile.sh
 ./run_tests.sh
-mv * $PBS_O_WORKDIR/.
+cp logfile.log errorfile.err $PBS_O_WORKDIR/
 rmdir $tempdir
