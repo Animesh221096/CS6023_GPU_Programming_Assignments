4d0f89da6be956beed98a8ec0b62f37205b0f0af
Animesh221096 <animeshjain221096@gmail.com>
2026-08-26 22:26:42 +0530

    Creating new kernal to use SharedMemory and Memory Coalescing

--- Assignment2/main.cu
+++ Assignment2/main.cu
@@ -1,53 +1,58 @@
-#include<iostream>
-#include<cstdio>
-#include<cstdlib>
-#include<sys/time.h>
-#include<cuda.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <sys/time.h>
+#include <cuda.h>
 using namespace std;
-
 
 #define BLOCKSIZE_X 16
 #define BLOCKSIZE_Y BLOCKSIZE_X
+#define TILE_WIDTH BLOCKSIZE_X
 
-__global__ void Mat_mul_1(const int *X, const int *Y, int* Z, 
-                        const int p, const int q, const int r){
-    // X is A^T (p x q)
-    // Y is B (q x r)  
-    // Z is result (p x r)
-    
-    int idx_x = blockIdx.x * blockDim.x + threadIdx.x;
-    int idx_y = blockIdx.y * blockDim.y + threadIdx.y;
+// __global__ void Mat_mul_1(const int *X, const int *Y, int* Z,
+//                         const int p, const int q, const int r){
+//     // X is A^T (p x q)
+//     // Y is B (q x r)
+//     // Z is result (p x r)
 
-    if(idx_x >= p || idx_y >= r){
-        return;
-    }
-    
-    for(int i = 0; i < q; i++){
-        Z[idx_x * r + idx_y] += X[i * p + idx_x] * Y[i * r + idx_y];
-    }
+//     int idx_x = blockIdx.x * blockDim.x + threadIdx.x;
+//     int idx_y = blockIdx.y * blockDim.y + threadIdx.y;
+
+//     if(idx_x >= p || idx_y >= r){
+//         return;
+//     }
+
+//     for(int i = 0; i < q; i++){
+//         Z[idx_x * r + idx_y] += X[i * p + idx_x] * Y[i * r + idx_y];
+//     }
+// }
+
+// __global__ void Mat_mul_2(const int *X, const int *Y, int* Z,
+//                         const int p, const int q, const int r){
+
+//     int idx_x = blockIdx.x * blockDim.x + threadIdx.x;
+//     int idx_y = blockIdx.y * blockDim.y + threadIdx.y;
+
+//     if(idx_x >= p || idx_y >= r){
+//         return;
+//     }
+
+//     for(int i = 0; i < q; i++){
+//         Z[idx_x * r + idx_y] += X[idx_x * q + i] * Y[idx_y * q + i];
+//     }
+// }
+
+__global__ void Mat_mul(const int *A, const int *B, const int *C,
+						const int *D, int *E,
+						const int p, const int q, const int r)
+{
+
 }
-
-__global__ void Mat_mul_2(const int *X, const int *Y, int* Z, 
-                        const int p, const int q, const int r){
-    
-    int idx_x = blockIdx.x * blockDim.x + threadIdx.x;
-    int idx_y = blockIdx.y * blockDim.y + threadIdx.y;
-
-    if(idx_x >= p || idx_y >= r){
-        return;
-    }
-    
-    for(int i = 0; i < q; i++){
-        Z[idx_x * r + idx_y] += X[idx_x * q + i] * Y[idx_y * q + i];
-    }
-}
-
-
-
 
 // function to compute the output matrix
 void compute(int p, int q, int r, int *h_matrixA, int *h_matrixB,
-	         int *h_matrixC, int *h_matrixD, int *h_matrixE){
+			 int *h_matrixC, int *h_matrixD, int *h_matrixE)
+{
 	// Device variables declarations...
 	int *d_matrixA, *d_matrixB, *d_matrixC, *d_matrixD, *d_matrixE;
 
@@ -64,18 +69,16 @@ void compute(int p, int q, int r, int *h_matrixA, int *h_matrixB,
 	cudaMemcpy(d_matrixC, h_matrixC, p * q * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_matrixD, h_matrixD, r * q * sizeof(int), cudaMemcpyHostToDevice);
 
-		/* ****************************************************************** */
-		/* Write your code here */
-		/* Configure and launch kernels */
+	/* ****************************************************************** */
+	/* Write your code here */
+	/* Configure and launch kernels */
 
-		cudaMemset(d_matrixE, 0, p * r * sizeof(int));
+	cudaMemset(d_matrixE, 0, p * r * sizeof(int));
 
-		dim3 blockSize(BLOCKSIZE_X, BLOCKSIZE_Y);
-		dim3 gridSize((p + BLOCKSIZE_X - 1) / BLOCKSIZE_Y, (r + BLOCKSIZE_Y - 1) / BLOCKSIZE_Y);
+	dim3 blockSize(BLOCKSIZE_X, BLOCKSIZE_Y);
+	dim3 gridSize((p + BLOCKSIZE_X - 1) / BLOCKSIZE_Y, (r + BLOCKSIZE_Y - 1) / BLOCKSIZE_Y);
 
-		Mat_mul_1<<<gridSize, blockSize>>>(d_matrixA, d_matrixB, d_matrixE, p, q, r);
-		Mat_mul_2<<<gridSize, blockSize>>>(d_matrixC, d_matrixD, d_matrixE, p, q, r);
-	
+	Mat_mul<<<gridSize, blockSize>>>(d_matrixA, d_matrixB, d_matrixC, d_matrixD, d_matrixE, p, q, r);
 
 	/* ****************************************************************** */
 
@@ -91,27 +94,32 @@ void compute(int p, int q, int r, int *h_matrixA, int *h_matrixB,
 }
 
 // function to read the input matrices from the input file
-void readMatrix(FILE *inputFilePtr, int *matrix, int rows, int cols) {
-	for(int i=0; i<rows; i++) {
-		for(int j=0; j<cols; j++) {
-			fscanf(inputFilePtr, "%d", &matrix[i*cols+j]);
+void readMatrix(FILE *inputFilePtr, int *matrix, int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			fscanf(inputFilePtr, "%d", &matrix[i * cols + j]);
 		}
 	}
 }
 
 // function to write the output matrix into the output file
-void writeMatrix(FILE *outputFilePtr, int *matrix, int rows, int cols) {
-	for(int i=0; i<rows; i++) {
-		for(int j=0; j<cols; j++) {
-			fprintf(outputFilePtr, "%d ", matrix[i*cols+j]);
+void writeMatrix(FILE *outputFilePtr, int *matrix, int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			fprintf(outputFilePtr, "%d ", matrix[i * cols + j]);
 		}
 		fprintf(outputFilePtr, "\n");
 	}
 }
 
-
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	// variable declarations
 	int p, q, r;
 	int *matrixA, *matrixB, *matrixC, *matrixD, *matrixE;
@@ -125,9 +133,10 @@ int main(int argc, char **argv) {
 	// file pointers
 	FILE *inputFilePtr, *outputFilePtr;
 
-    inputFilePtr = fopen(inputFileName, "r");
-	if(inputFilePtr == NULL) {
-	    printf("Failed to open the input file.!!\n");
+	inputFilePtr = fopen(inputFileName, "r");
+	if (inputFilePtr == NULL)
+	{
+		printf("Failed to open the input file.!!\n");
 		return 0;
 	}
 
@@ -135,17 +144,17 @@ int main(int argc, char **argv) {
 	fscanf(inputFilePtr, "%d %d %d", &p, &q, &r);
 
 	// allocate memory and read input matrices
-	matrixA = (int*) malloc(q * p * sizeof(int));
-	matrixB = (int*) malloc(q * r * sizeof(int));
-	matrixC = (int*) malloc(p * q * sizeof(int));
-	matrixD = (int*) malloc(r * q * sizeof(int));
+	matrixA = (int *)malloc(q * p * sizeof(int));
+	matrixB = (int *)malloc(q * r * sizeof(int));
+	matrixC = (int *)malloc(p * q * sizeof(int));
+	matrixD = (int *)malloc(r * q * sizeof(int));
 	readMatrix(inputFilePtr, matrixA, q, p);
 	readMatrix(inputFilePtr, matrixB, q, r);
 	readMatrix(inputFilePtr, matrixC, p, q);
 	readMatrix(inputFilePtr, matrixD, r, q);
 
 	// allocate memory for output matrix
-	matrixE = (int*) malloc(p * r * sizeof(int));
+	matrixE = (int *)malloc(p * r * sizeof(int));
 
 	// call the compute function
 	gettimeofday(&t1, NULL);
@@ -156,7 +165,7 @@ int main(int argc, char **argv) {
 	// print the time taken by the compute function
 	seconds = t2.tv_sec - t1.tv_sec;
 	microSeconds = t2.tv_usec - t1.tv_usec;
-	printf("Time taken (ms): %.3f\n", 1000*seconds + microSeconds/1000);
+	printf("Time taken (ms): %.3f\n", 1000 * seconds + microSeconds / 1000);
 
 	// store the result into the output file
 	outputFilePtr = fopen(outputFileName, "w");
